19cd558def3f26d56e7cead0384c0848e21ec5b9
Igor correa <icc@pop-os.localdomain>
2021-06-13 23:49:41 -0300

    first commit (projeto portifolio 002)

---
+++ src/Icc-theme.js
@@ -0,0 +1,19 @@
+const IccTheme = function () {
+  const btnFlip = document.querySelector(".container_img");
+  const _html = document.querySelector("html");
+  const _title = document.querySelector("h1");
+
+  btnFlip.addEventListener("click", function () {
+    btnFlip.classList.toggle("switch-mode");
+
+    if (_html.classList.contains("light")) {
+      _title.textContent = `Trocando o Tema do Site (tema light)`;
+      _html.setAttribute("class", "dark");
+    } else {
+      _title.textContent = `Trocando o Tema do Site (tema dark)`;
+      _html.setAttribute("class", "light");
+    }
+  });
+};
+
+export default IccTheme;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "../style/style.css";
+import IccTheme from "./Icc-theme.js";
+
+IccTheme();
