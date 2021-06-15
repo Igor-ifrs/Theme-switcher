e5666f6a2b7372e2aa1062ff5ba73265139f8777
Igor correa <icc@pop-os.localdomain>
2021-06-15 18:42:54 -0300

    removendo js desnecessario (projeto portifolio 002)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import "../style/style.css";
-import IccTheme from "./Icc-theme.js";
 
-IccTheme();
+const IccTheme = function () {
+  const btnFlip = document.querySelector(".container_img");
+  const _html = document.querySelector("html");
+  const _title = document.querySelector("h1");
+
+  btnFlip.addEventListener("click", function () {
+    btnFlip.classList.toggle("switch-mode");
+
+    if (_html.classList.contains("light")) {
+      _title.textContent = `Trocando o Tema do Site (tema light)`;
+      _html.setAttribute("class", "dark");
+    } else {
+      _title.textContent = `Trocando o Tema do Site (tema dark)`;
+      _html.setAttribute("class", "light");
+    }
+  });
+};
+
+window.addEventListener("load", IccTheme);
